b3d77ab206d9b3bd5f9020c682881917449e4dcf
Yunxing Dai <yunxing@cloudwalk.me>
2016-08-19 16:24:40 -0700

    init commit

---
+++ src/bus.ml
@@ -0,0 +1,88 @@
+open! Core.Std
+open! Import
+
+module Bus = Core_kernel.Bus
+include Bus
+
+let pipe1_exn (t : ('a -> unit) Read_only.t) here =
+  let r, w = Pipe.create () in
+  let subscription =
+    subscribe_exn t here ~f:(function v ->
+      Pipe.write_without_pushback_if_open w v)
+  in
+  upon (Pipe.closed w) (fun () -> unsubscribe t subscription);
+  r
+;;
+
+module First_arity = struct
+  type (_, _, _) t =
+    | Arity1 : ('a ->                   unit, 'a ->                   'r option, 'r) t
+    | Arity2 : ('a -> 'b ->             unit, 'a -> 'b ->             'r option, 'r) t
+    | Arity3 : ('a -> 'b -> 'c ->       unit, 'a -> 'b -> 'c ->       'r option, 'r) t
+    | Arity4 : ('a -> 'b -> 'c -> 'd -> unit, 'a -> 'b -> 'c -> 'd -> 'r option, 'r) t
+  [@@deriving sexp_of]
+end
+
+let first_exn (type c) (type f) (type r)
+      t here (first_arity : (c, f, r) First_arity.t) ~(f : f) =
+  let module A = First_arity in
+  Deferred.create (fun ivar ->
+    let subscriber : c Bus.Subscriber.t option ref = ref None in
+    let finish : r option -> unit = function
+      | None -> ()
+      | Some r ->
+        Ivar.fill ivar r;
+        Bus.unsubscribe t (Option.value_exn !subscriber);
+    in
+    subscriber :=
+      Some
+        (Bus.subscribe_exn t here
+           ~on_callback_raise:(let monitor = Monitor.current () in
+                               fun error -> Monitor.send_exn monitor (Error.to_exn error))
+           ~f:(match first_arity with
+             | A.Arity1 -> (fun a           -> finish (f a)          )
+             | A.Arity2 -> (fun a1 a2       -> finish (f a1 a2)      )
+             | A.Arity3 -> (fun a1 a2 a3    -> finish (f a1 a2 a3)   )
+             | A.Arity4 -> (fun a1 a2 a3 a4 -> finish (f a1 a2 a3 a4)))))
+;;
+
+let%test_unit "first_exn" =
+  let bus =
+    Bus.create [%here]
+      Arity1
+      ~allow_subscription_after_first_write:true
+      ~on_callback_raise:Error.raise
+  in
+  let d = first_exn (Bus.read_only bus) [%here] Arity1 ~f:(fun _ -> Some ()) in
+  [%test_result: int] (Bus.num_subscribers bus) ~expect:1;
+  Bus.write bus 0;
+  assert (Deferred.is_determined d);
+  [%test_result: int] (Bus.num_subscribers bus) ~expect:0;
+  let d =
+    first_exn (Bus.read_only bus) [%here] Arity1
+      ~f:(fun i -> if i = 13 then Some () else None)
+  in
+  Bus.write bus 12;
+  assert (not (Deferred.is_determined d));
+  [%test_result: int] (Bus.num_subscribers bus) ~expect:1;
+  Bus.write bus 13;
+  assert (Deferred.is_determined d)
+;;
+
+let%test_unit "first_exn ~f raises" =
+  let bus =
+    Bus.create [%here]
+      Arity1
+      ~allow_subscription_after_first_write:true
+      ~on_callback_raise:Error.raise
+  in
+  let monitor = Monitor.create () in
+  Monitor.detach monitor;
+  let d =
+    within' ~monitor (fun () ->
+      first_exn (Bus.read_only bus) [%here] Arity1 ~f:(fun _ -> failwith ""))
+  in
+  Bus.write bus 0;
+  assert (not (Deferred.is_determined d));
+  assert (Monitor.has_seen_error monitor);
+;;
---
+++ src/command.mli
@@ -0,0 +1,38 @@
+(** [Async.Command] is [Core.Command] with additional Async functions. *)
+
+open! Import
+
+include module type of Core.Std.Command
+  with type t      = Core.Std.Command.t
+  with module Spec = Core.Std.Command.Spec
+
+(** [async] is like [Core.Command.basic], except that the main function it expects returns
+    [unit Deferred.t], instead of [unit].  [async] will also start the Async scheduler
+    before main is run, and will stop the scheduler when main returns. *)
+val async  : ('a, unit Deferred.t) basic_command
+val async' : unit Deferred.t basic_command'
+
+(** [async_basic] is a deprecated synonym for [async] that will eventually go away.  It is
+    here to give code outside jane street a chance to switch over before we delete it. *)
+val async_basic : ('a, unit Deferred.t) basic_command
+  [@@ocaml.deprecated "[since 2015-10] Use async instead"]
+
+(** [async_or_error] is like [async], except that the main function it expects may
+    return an error, in which case it prints out the error message and shuts down with
+    exit code 1. *)
+val async_or_error  : ('a, unit Deferred.Or_error.t) basic_command
+val async_or_error' : unit Deferred.Or_error.t basic_command'
+
+(** To create an [Arg_type.t] that uses auto-completion and uses Async to compute the
+    possible completions, one should use
+
+    {[
+      Arg_type.create ~complete of_string
+    ]}
+
+    where [complete] wraps its Async operations in [Thread_safe.block_on_async].  With
+    this, the [complete] function is only called when the executable is auto-completing,
+    not for ordinary execution.  This improves performance, and also means that the Async
+    scheduler isn't started for ordinary execution of the command, which makes it possible
+    for the command to daemonize (which requires the scheduler to not have been started).
+*)
